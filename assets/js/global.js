69029a26ff91b01e38d1e12049dd44c9b9bf6b55
David Ponce Vargas <buap.david@gmail.com>
2020-07-23 11:59:21 -0500

    fix links from navbar

--- assets/js/global.js
+++ assets/js/global.js
@@ -6,37 +6,37 @@ textFooter.textContent += new Date().getFullYear();
 
 const dropdown = `
       <div class="dropdown">
-        <a href="#" class="dropdown-item">
+        <a href="http://carlospereyra.edu.mx/MedioAmbienteFCC/index.html" class="dropdown-item">
           <div class="dropdown-icon">
             <img src="/medio-ambiente-fcc/assets/images/home.svg" alt="inicio" />
           </div>
           <span>Inicio</span>
         </a>
-        <a href="#" class="dropdown-item">
+        <a href="http://carlospereyra.edu.mx/MedioAmbienteFCC/about-us.html" class="dropdown-item">
           <div class="dropdown-icon">
             <img src="/medio-ambiente-fcc/assets/images/about.svg" alt="Acerca de nostros" />
           </div>
           <span>Acerca de nostros</span>
         </a>
-        <a href="#" class="dropdown-item">
+        <a href="http://carlospereyra.edu.mx/MedioAmbienteFCC/articles.html" class="dropdown-item">
           <div class="dropdown-icon">
             <img src="/medio-ambiente-fcc/assets/images/articles.svg" alt="Artículos" />
           </div>
           <span>Artículos</span>
         </a>
-        <a href="#" class="dropdown-item dropdown-item--active">
+        <a href="/medio-ambiente-fcc/suma-fcc" class="dropdown-item dropdown-item--active">
           <div class="dropdown-icon">
             <img src="/medio-ambiente-fcc/assets/images/events.svg" alt="SuMA" />
           </div>
           <span>SuMA</span>
         </a>
-        <a href="#" class="dropdown-item">
+        <a href="http://carlospereyra.edu.mx/MedioAmbienteFCC/sitemap.html" class="dropdown-item">
           <div class="dropdown-icon">
             <img src="/medio-ambiente-fcc/assets/images/gallery.svg" alt="Galeria" />
           </div>
           <span>Galeria</span>
         </a>
-        <a href="#" class="dropdown-item">
+        <a href="http://carlospereyra.edu.mx/MedioAmbienteFCC/sitemap.html" class="dropdown-item">
           <div class="dropdown-icon">
             <img src="/medio-ambiente-fcc/assets/images/map.svg" alt="Mapa del sitio" />
           </div>
